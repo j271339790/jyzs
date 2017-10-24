36a1d7a3f31fc7715faec127ee58d36bd0336b6a
j271339790 <30430788+j271339790@users.noreply.github.com>
2017-10-24 12:23:08 +0800

    Add files via upload

---
+++ json/sj.js
@@ -0,0 +1,21 @@
+var data = [
+	{
+		"title":"HTML",
+		"uname":"前端初识之HTML以及常用标签",
+		"url":"http://jzhuang.ltd/Andy/Andy.html",
+		"time":"2016-8-16"
+	},
+	{
+		"title":"form表单",
+		"uname":"前端初识之HTML->form表单",
+		"url":"http://jzhuang.ltd/Andy/form.html",
+		"time":"2016-8-16"
+	},
+	{
+		"title":"初识CSS",
+		"uname":"初识CSS",
+		"url":"http://jzhuang.ltd/Andy/css-1.html",
+		"time":"2016-8-20"
+	}
+]
+
